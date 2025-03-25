9c544fc8bce1153538dccd5f5eb8dc78d42ec4c2
aiiti-origami <gotoukaeru@gmail.com>
2025-03-25 16:32:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 function myLanguage(code) {
     let buttonIndex = 0; // ボタンごとにユニークなIDを作る
-    code = code.replace(/表示\("(.+)"\)/g, 'console.log("$1"); document.getElementById("output").innerHTML += "$1<br>";');
+    let variables = {}; // 変数の格納場所
 
-    // 「ボタン」を JavaScript に変換
-    code = code.replace(/ボタン\("(.+)"\) \{([^}]+)\}/g, function(match, btnText, btnAction) {
-        let btnId = "btn" + buttonIndex++;
-        let jsAction = myLanguage(btnAction); // ボタン内のコードも変換
-        return `
-            document.getElementById("output").innerHTML += '<button id="${btnId}">${btnText}</button>';
-            setTimeout(() => {
-                document.getElementById("${btnId}").onclick = function() { ${jsAction} };
-            }, 10);
-        `;
+    // 「表示("文字")」を JavaScript に変換
+    code = code.replace(/表示\("([^"]+)"\);/g, 'console.log("$1"); document.getElementById("output").innerHTML += "$1<br>";');
+
+    // 「表示(変数)」を JavaScript に変換
+    code = code.replace(/表示\(([^)]+)\);/g, function(match, varName) {
+        if (variables[varName] !== undefined) {
+            return `console.log(${variables[varName]}); document.getElementById("output").innerHTML += ${variables[varName]} + "<br>";`;
+        }
+        return match; // 変換できない場合はそのまま
+    });
+
+    // 「数値 変数名 = 数字;」を JavaScript に変換（変数を保存する）
+    code = code.replace(/数値 ([^=]+) = ([0-9]+);/g, function(match, varName, value) {
+        variables[varName] = value; // 変数を保存
+        return `let ${varName} = ${value};`;
+    });
+
+    // 「変数名 = 数字;」を JavaScript に変換（変数に新しい値を代入）
+    code = code.replace(/([^=]+) = ([0-9]+);/g, function(match, varName, value) {
+        if (variables[varName] !== undefined) {
+            variables[varName] = value; // 変数に新しい値を代入
+            return `${varName} = ${value};`;
+        }
+        return match; // 変換できない場合はそのまま
     });
 
     return code;
