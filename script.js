ffb13305f934c3e15c10edd063656f0f9550826f
aiiti-origami <gotoukaeru@gmail.com>
2025-03-25 16:44:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
 function myLanguage(code) {
     let buttonIndex = 0; // ボタンごとにユニークなIDを作る
-    let variables = {}; // 変数の格納場所
+    code = code.replace(/表示\("(.+)"\)/g, 'console.log("$1"); document.getElementById("output").innerHTML += "$1<br>";');
 
-    // 「表示("文字")」を JavaScript の console.log と HTML への表示に変換
-    code = code.replace(/表示\("([^"]+)"\);/g, 'console.log("$1"); document.getElementById("output").innerHTML += "$1<br>";');
-
-    // 「表示(変数)」を JavaScript に変換
-    code = code.replace(/表示\(([^)]+)\);/g, function(match, varName) {
-        if (variables[varName] !== undefined) {
-            return `console.log(${variables[varName]}); document.getElementById("output").innerHTML += ${variables[varName]} + "<br>";`;
-        }
-        return match; // 変換できない場合はそのまま
-    });
-
-    // 「数値 変数名 = 数字;」を JavaScript に変換（変数を保存する）
-    code = code.replace(/数値 ([^=]+) = ([0-9]+);/g, function(match, varName, value) {
-        variables[varName] = value; // 変数を保存
-        return `let ${varName} = ${value};`;
-    });
-
-    // 「変数名 = 数字;」を JavaScript に変換（変数に新しい値を代入）
-    code = code.replace(/([^=]+) = ([0-9]+);/g, function(match, varName, value) {
-        if (variables[varName] !== undefined) {
-            variables[varName] = value; // 変数に新しい値を代入
-            return `${varName} = ${value};`;
-        }
-        return match; // 変換できない場合はそのまま
-    });
-
-    // ボタンの処理（ボタンを作ってイベントを追加）
     code = code.replace(/ボタン\("(.+)"\) \{([^}]+)\}/g, function(match, btnText, btnAction) {
         let btnId = "btn" + buttonIndex++;
         let jsAction = myLanguage(btnAction); // ボタン内のコードも変換
@@ -46,12 +19,6 @@ function myLanguage(code) {
 function runCode() {
     let inputCode = document.getElementById("codeInput").value;
     let jsCode = myLanguage(inputCode);
-    
     document.getElementById("output").innerHTML = ""; // 実行前にリセット
-    try {
-        eval(jsCode); // JavaScript に変換したコードを実行
-    } catch (e) {
-        document.getElementById("output").innerHTML = "エラー: " + e.message;
-        console.error(e);
-    }
+    eval(jsCode);
 }
